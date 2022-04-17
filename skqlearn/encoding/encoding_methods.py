7c02afb0e2c21a623484029489665bbc36196a4c
Daniel Mohedano <danimohedano1998@gmail.com>
2022-04-17 13:33:29 +0200

    Implemented amplitude encoding (regular and expanded)

--- skqlearn/encoding/encoding_methods.py
+++ skqlearn/encoding/encoding_methods.py
@@ -66,12 +66,132 @@ def _basis_encoding_dataset(x: np.ndarray) -> np.ndarray:
     size = int(np.ceil(np.log2(max_data+1))**2)
     state = np.zeros(size)
 
-    for data in x:
+    for i in range(x.shape[0]):
         # Addition of every state to create superposition
-        state += _basis_encoding_single(data, size)
+        state += _basis_encoding_single(x[i], size)
 
     # Normalization of the state
     state /= np.sqrt(x.shape[0])
 
     return state
 
+
+def amplitude_encoding(x: np.ndarray) -> np.ndarray:
+    """Application of amplitude encoding to the input.
+
+    Args:
+        x (np.ndarray of shape (n_features,) or (n_samples, n_features)):
+            Input. This can be a single sample of shape (n_features,) or a
+            dataset of shape (n_samples, n_features). The input MUST be
+            normalized. If it is not, use amplitude_encoding_expanded()
+            instead.
+
+    Returns:
+        np.ndarray: Quantum state described as an amplitude vector. If a
+            dataset is provided, the states are concatenated.
+
+    Raises:
+        ValueError: If an invalid input is provided.
+    """
+    if len(x.shape) == 1:
+        return _amplitude_encoding_single(x)
+    elif len(x.shape) == 2:
+        return _amplitude_encoding_dataset(x)
+    else:
+        raise ValueError(f'Invalid input shape provided. Expected 1D or 2D, '
+                         f'got {x.shape} instead.')
+
+
+def _amplitude_encoding_single(x: np.ndarray) -> np.ndarray:
+    """Application of amplitude encoding to a single sample.
+
+    Args:
+        x (np.ndarray of shape (n_features,)): Input sample.
+
+    Returns:
+        np.ndarray: Quantum state described as an amplitude vector.
+    """
+    size = int(np.ceil(np.log2(x.shape[0]))**2)
+
+    # Return array with padded 0s at the end
+    return np.pad(x, (0, size - x.shape[0]))
+
+
+def _amplitude_encoding_dataset(x: np.ndarray) -> np.ndarray:
+    """Application of amplitude encoding to a dataset.
+
+    Args:
+        x (np.ndarray of shape (n_samples, n_features)): Input dataset.
+
+    Returns:
+        np.ndarray: Quantum state described as an amplitude vector constructed
+            by concatenating the quantum states for each sample.
+    """
+    states = []
+    for i in range(x.shape[0]):
+        states += _amplitude_encoding_single(x[i, :]).tolist()
+
+    return np.array(states)
+
+
+def amplitude_encoding_expanded(x: np.ndarray) -> np.ndarray:
+    """Application of expanded amplitude encoding to the input.
+
+    Expanded amplitude encoding allows for non-normalized vector to be encoded
+    as quantum states. This is achieved by the inclusion of an extra feature
+    with value 1.
+
+    todo: elaborate on explanation
+
+    Args:
+        x (np.ndarray of shape (n_features,) or (n_samples, n_features)):
+            Input. This can be a single sample of shape (n_features,) or a
+            dataset of shape (n_samples, n_features).
+
+    Returns:
+        np.ndarray: Quantum state described as an amplitude vector. If a
+            dataset is provided, the states are concatenated.
+
+    Raises:
+        ValueError: If an invalid input is provided.
+    """
+    if len(x.shape) == 1:
+        return _amplitude_encoding_expanded_single(x)
+    elif len(x.shape) == 2:
+        return _amplitude_encoding_expanded_dataset(x)
+    else:
+        raise ValueError(f'Invalid input shape provided. Expected 1D or 2D, '
+                         f'got {x.shape} instead.')
+
+
+def _amplitude_encoding_expanded_single(x: np.ndarray) -> np.ndarray:
+    """Application of expanded amplitude encoding to a single sample.
+
+    Args:
+        x (np.ndarray of shape (n_features,)): Input sample.
+
+    Returns:
+        np.ndarray: Quantum state described as an amplitude vector.
+    """
+    # Encode the vector with an extra feature of value 1.0
+    state = _amplitude_encoding_single(np.pad(x, (0, 1), constant_values=1.0))
+
+    # Normalize the vector to make it a viable quantum state
+    return state / np.linalg.norm(state)
+
+
+def _amplitude_encoding_expanded_dataset(x: np.ndarray) -> np.ndarray:
+    """Application of amplitude encoding to a dataset.
+
+    Args:
+        x (np.ndarray of shape (n_samples, n_features)): Input dataset.
+
+    Returns:
+        np.ndarray: Quantum state described as an amplitude vector constructed
+            by concatenating the quantum states for each sample.
+    """
+    states = []
+    for i in range(x.shape[0]):
+        states += _amplitude_encoding_expanded_single(x[i, :]).tolist()
+
+    return np.array(states)
