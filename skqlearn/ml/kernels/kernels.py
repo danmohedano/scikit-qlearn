bb8fcddda855cfecd8108c28a80f62042151d376
Daniel Mohedano <danimohedano1998@gmail.com>
2022-06-03 12:55:27 +0200

    Implemented SqueezingKernel

--- skqlearn/ml/kernels/kernels.py
+++ skqlearn/ml/kernels/kernels.py
@@ -2,7 +2,41 @@ import numpy as np
 
 
 class SqueezingKernel:
-    """SqueezingKernel :cite:`hilbert2019`"""
+    r"""Kernel inspired by squeezed vacuum states :cite:`hilbert2019`
+
+    It is based on the definition of a *squeezed vacuum state* of the
+    electromagnetic field as:
+
+    .. math::
+       \left|z\right> = \frac{1}{\sqrt{\cosh(r)}}
+       \sum_{n=0}^{\infty}\frac{\sqrt{(2n)!}}{2^n n!}
+       (-e^{i\varphi}\tanh(r))\left|2n\right>
+
+    The notation :math:`\left|z\right> = \left|(r, \varphi)\right>` is also
+    used, where :math:`z=re^{i\varphi}`.
+
+    :math:`x\rightarrow\left|\phi(x)\right>=\left|(c,x)\right>` can be
+    interpreted as a feature map for :math:`x\in\mathbb{R}`. The hyperparameter
+    :math:`c` determines the strength of the squeezing. This can be extended
+    to vectors of the form
+    :math:`\boldsymbol{x}=(x_1, ..., x_N)^T\in\mathbb{R}^N`,
+    defining the feature map as:
+
+    .. math::
+       \phi: \boldsymbol{x} \rightarrow \left|(c,\boldsymbol{x})\right>=
+       \left|(c,x_1)\right> \bigotimes ... \bigotimes\left|(c,x_N)\right>
+
+    It therefore defines the following kernel:
+
+    .. math::
+       k(\boldsymbol{x}, \boldsymbol{x}')=\prod_{i=1}^{N} \left<(c,x_i)|
+       (c, x_i')\right>=\prod_{i=1}^N\sqrt{\frac{\text{sech }c\text{ sech }c}
+       {1-e^{i(x_i'-x_i)}\tanh c \tanh c}}
+
+    This can be evaluated classically by taking the absolute square of the
+    inner product.
+
+    """
     def __init__(self, c: float):
         """
 
